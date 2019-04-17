82bad75b7bae1586cdae745e498a120c8b86a0e2
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-17 05:10:31 +0000

    Cluster for all DevStats project will require more power
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/cncfekscluster.sh
+++ scripts/cncfekscluster.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-AWS_PROFILE=cncf eksctl create cluster --name=CNCFcluster --nodes=4 --kubeconfig=/root/.kube/config_cncf --tags node=devstats --node-volume-type=gp2 --node-volume-size=50 --version=1.12 --nodegroup-name=CNCFnodegroup --node-type=m5.xlarge --node-ami=auto --ssh-access
+#AWS_PROFILE=cncf eksctl create cluster --name=CNCFcluster --nodes=4 --kubeconfig=/root/.kube/config_cncf --tags node=devstats --node-volume-type=gp2 --node-volume-size=50 --version=1.12 --nodegroup-name=CNCFnodegroup --node-type=m5.xlarge --node-ami=auto --ssh-access
+AWS_PROFILE=cncf eksctl create cluster --name=CNCFcluster --nodes=8 --kubeconfig=/root/.kube/config_cncf --tags node=devstats --node-volume-type=gp2 --node-volume-size=50 --version=1.12 --nodegroup-name=CNCFnodegroup --node-type=m5.2xlarge --node-ami=auto --ssh-access
