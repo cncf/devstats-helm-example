486cd5080425b9633927ee9ec5c3be8e964c13ec
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2019-04-24 15:10:04 +0000

    Config update
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- scripts/delete_all.sh
+++ scripts/delete_all.sh
@@ -9,6 +9,7 @@ kubectl delete secret pg-db github-oauth grafana-secret
 ../devstats-k8s-lf/util/delete_objects.sh configmap 'devstats-'
 ../devstats-k8s-lf/util/delete_objects.sh pod 'devstats-'
 ../devstats-k8s-lf/util/delete_objects.sh pvc 'pgdata-devstats-'
+../devstats-k8s-lf/util/delete_objects.sh pvc 'devstats-pvc-'
 ../devstats-k8s-lf/util/delete_objects.sh rolebinding 'devstats-'
 ../devstats-k8s-lf/util/delete_objects.sh role 'devstats-'
 ../devstats-k8s-lf/util/delete_objects.sh serviceaccount 'devstats-'
